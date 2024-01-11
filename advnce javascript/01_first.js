2ee708cf967b9f6549375adcfb94f1c0bbcff43e
thesiyahemali999@gmal.com <thesiyahemali999@gmal.com>
2024-01-11 11:45:26 +0530

    update

---
+++ advnce javascript/01_first.js
@@ -0,0 +1,9 @@
+// const {sum,a} = require('./02_seconde.js')
+// sum(2,6)
+// console.log(a);
+
+// const {oddeven}=require('./02_seconde.js')
+// oddeven(4)
+
+const {a}=require('./02_seconde.js')
+a(2)
---
+++ advnce javascript/02_seconde.js
@@ -0,0 +1,22 @@
+// let a=5;
+// const sum = (a,b) => {
+//  console.log(a+b);
+// }
+// module.exports = {sum,a}
+
+// example2
+// function oddeven(a){
+//     if(a%2==0){
+//         console.log('this is even number');
+//     }
+//     else{
+//         console.log('this is odd number');
+//     }
+// }
+// module.exports={oddeven}
+
+// example 3
+function squareroot(a){
+   Math.SQRT2(a)
+}
+module.exports={a}
